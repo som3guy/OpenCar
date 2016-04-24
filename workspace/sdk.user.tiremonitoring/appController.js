cf506f8828494234cf04d8e2d2bb24fe880e2a1e
Jeremy Miller <jeremy.j.miller@cerner.com>
2016-04-24 08:24:07 -0500

    adding 4 tires to tpms

--- workspace/sdk.user.tiremonitoring/appController.js
+++ workspace/sdk.user.tiremonitoring/appController.js
@@ -10,6 +10,15 @@ define(function(require) {
     var onTirePFR = function(tirePFR) {
         view.displayTirePFR(tirePFR * 0.14503773773020923); //kPa to psi
     };
+    var onTirePFL = function(tirePFL) {
+        view.displayTirePFL(tirePFL * 0.14503773773020923); //kPa to psi
+    };
+    var onTirePRR = function(tirePRR) {
+        view.displayTirePRR(tirePRR * 0.14503773773020923); //kPa to psi
+    };
+    var onTirePRL = function(tirePRL) {
+        view.displayTirePRL(tirePRL * 0.14503773773020923); //kPa to psi
+    };
     //
     // This is the application "controller" module.
     //
@@ -23,9 +32,21 @@ define(function(require) {
             beforeStart: function($super) {
                 telematics = new TelematicsAPI();
                 telematics.subscribe(TelematicsAPI.Event.TIRE_PRESSURE_FRONT_RIGHT, onTirePFR);
+                telematics.subscribe(TelematicsAPI.Event.TIRE_PRESSURE_FRONT_LEFT, onTirePFL);
+                telematics.subscribe(TelematicsAPI.Event.TIRE_PRESSURE_REAR_RIGHT, onTirePRR);
+                telematics.subscribe(TelematicsAPI.Event.TIRE_PRESSURE_REAR_LEFT, onTirePRL);
                 view = this.getView();
                 telematics.getTirePressureFrontRight().done(function(tirePFR) {
                     onTirePFR(tirePFR);
+                });
+                telematics.getTirePressureFrontLeft().done(function(tirePFL) {
+                    onTirePFL(tirePFL);
+                });
+                telematics.getTirePressureRearRight().done(function(tirePRR) {
+                    onTirePRR(tirePRR);
+                });
+                telematics.getTirePressureRearLeft().done(function(tirePRL) {
+                    onTirePRL(tirePRL);
                 });
                 return $super();
             }
--- workspace/sdk.user.tiremonitoring/appView.js
+++ workspace/sdk.user.tiremonitoring/appView.js
@@ -14,6 +14,24 @@ define(function(require) {
                 var control = Control.getById("tirePressureFrontRight");
                 this.txtView = Control.getById("tirePressureFrontRight");
                 control.model.set('text', tirePFR.toFixed(1));
+            },
+            displayTirePFL: function(tirePFL) {
+                this.txtView = Control.getById("tirePressureFrontLeft");
+                var control = Control.getById("tirePressureFrontLeft");
+                this.txtView = Control.getById("tirePressureFrontLeft");
+                control.model.set('text', tirePFL.toFixed(1));
+            },
+            displayTirePRR: function(tirePRR) {
+                this.txtView = Control.getById("tirePressureRearRight");
+                var control = Control.getById("tirePressureRearRight");
+                this.txtView = Control.getById("tirePressureRearRight");
+                control.model.set('text', tirePRR.toFixed(1));
+            },
+            displayTirePRL: function(tirePRL) {
+                this.txtView = Control.getById("tirePressureRearLeft");
+                var control = Control.getById("tirePressureRearLeft");
+                this.txtView = Control.getById("tirePressureRearLeft");
+                control.model.set('text', tirePRL.toFixed(1));
             }
         }
     );
